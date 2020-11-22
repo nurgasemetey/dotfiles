e7eac61c2480a1383185c066f785e251c4ff3002
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2020-11-22 16:44:47 +0300

    Update DEPRECATED-getslack.sh

--- DEPRECATED-getslack.sh
+++ DEPRECATED-getslack.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 alias setclip='xclip -selection c'
 alias getclip='xclip -selection clipboard -o'
-printf "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"Finished\"}' https://hooks.slack.com/services/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" | setclip
+printf "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"Finished\"}' $HOOK_URL | setclip
