9fbf73d963661dfe78fe152e534a9ca735f28d12
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2021-02-08 12:24:09 +0300

    update

--- worry-notes.sh
+++ worry-notes.sh
@@ -3,11 +3,11 @@ text=$(zenity --entry --title "WorryBox" --text "Enter text")
 echo "$text"
 d=`date +%Y-%m-%d`
 echo $d
-articlePath="/mnt/fd68f224-e30e-4ea0-8bd8-7f2a4c4d5ab0/nurgasemetey-environment/personal-notes/worries"
+articlePath="/mnt/fd68f224-e30e-4ea0-8bd8-7f2a4c4d5ab0/nurgasemetey-environment/personal-notes/worries/"
 
 if [ -z "$text" ]
 then
       echo "Empty text"
 else
-	printf "* $text\n" >> "$articlePath$d.md"
+	printf "$text\n" >> "$articlePath$d.txt"
 fi
