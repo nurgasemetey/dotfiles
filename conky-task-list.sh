6234b53fe6c4dc9561e2a63d4bf55f104ff48254
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2021-04-20 13:40:24 +0300

    update

---
+++ conky-task-list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MONTH_NAME=$(date +%B | tr '[:lower:]' '[:upper:]')
+# echo $MONTH_NAME
+MONTH=$(date +%m)
+# echo $MONTH
+DATE=$(date +'%Y-%m-%d')
+articlePath="/mnt/fd68f224-e30e-4ea0-8bd8-7f2a4c4d5ab0/nurgasemetey-environment/personal-notes/want-to-know/$MONTH-$MONTH_NAME/$MONTH_NAME-TASKS.md"
+# echo $articlePath
+# echo $DATE
+sed -e '1,/'$DATE'/ d' $articlePath
