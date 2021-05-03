066facf348eaba9de72cf3403cd57859933a581e
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2021-05-03 17:39:39 +0300

    update

--- intention-asker-v2.sh
+++ intention-asker-v2.sh
@@ -1,5 +1,4 @@
-echo SCREEN_UNLOCKED;
-text=$(zenity --entry --title "Laterbox" --text "Enter your INTENTION")
+text=$(zenity --entry --title "Intention" --text "Enter your INTENTION")
 echo "$text"
 articlePath="/mnt/fd68f224-e30e-4ea0-8bd8-7f2a4c4d5ab0/nurgasemetey-environment/personal-notes/tmp-storage/intentions.txt"
 if [ -z "$text" ]
